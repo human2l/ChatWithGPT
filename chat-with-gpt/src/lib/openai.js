259aea8d7cc143324edd8a44d7a0f03e57d97c6a
Kai <human2lkai@gmail.com>
2023-03-16 22:23:30 +1100

    Create openai.js

---
+++ chat-with-gpt/src/lib/openai.js
@@ -0,0 +1,19 @@
+const { DUMMY_DATA } = require("@/constances");
+const { Configuration, OpenAIApi } = require("openai");
+
+const { Configuration, OpenAIApi } = require("openai");
+
+const configuration = new Configuration({
+  apiKey: process.env.OPENAI_API_KEY,
+});
+const openai = new OpenAIApi(configuration);
+
+const getCompletion = async (dialogue) => {
+  const completion = await openai.createChatCompletion({
+    model: "gpt-3.5-turbo",
+    messages: dialogue, //[{role: "user", content: "Hello world"}]
+  });
+};
+
+// console.log(completion.data.choices[0].message);
+export default getCompletion;
